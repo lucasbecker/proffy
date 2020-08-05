9f618c733926cca46b0ab57bbb84757025b76a2b
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-08-05 15:47:56 -0300

    Third day update
    Finalização do frontend web, criação de novos componentes, integração do frontend web com o backend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,52 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id
+    })
+  }
+
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/15279481?s=460&u=9807bda4999f308a1f56e10842e2aca853f112a4&v=4" alt="Lucas Becker"/>
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Lucas Becker</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de física avançada.
-        <br /> <br />
-        Apaixonado por quedas livres em laboratório e por mudar a vida das pessoas através de experiências.
-      </p>
+      <p> {teacher.bio} </p>
       <footer>
         <p>
           Preço/Hora
-          <strong>R$ 25,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`} 
+          target="_blank" 
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
